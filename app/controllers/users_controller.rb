9af18a8c3dbc38065b0ce77859cbee51d27c1c34
Rob <robertipk@gmail.com>
2016-01-03 01:18:47 -0500

    added infowindow to markers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
+      marker.infowindow user.address
     end
   end
 
