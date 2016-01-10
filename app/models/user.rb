da74638d35cbcc960c4d6619d5f1479c2567e47d
Rob <robertipk@gmail.com>
2016-01-09 22:11:52 -0500

    added several more cities

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
 	geocoded_by :address
 	after_validation :geocode
+
+	def self.get_weather
+		ForecastIO.api_key = 'dc105edc628388d692f7af6a159918d7'
+        forecast = ForecastIO.forecast(latitude, longitude)
+        current_weather = forecast[:currently].summary
+        temperature = forecast[:currently].temperature
+        timezone = forecast[:timezone]
+        offset = forecast[:offset]
+        time = Time.at(forecast[:currently].time)
+        save
+    end
 end
